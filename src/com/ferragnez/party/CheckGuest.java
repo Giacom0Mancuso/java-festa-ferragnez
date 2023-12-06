8ac40b18a014d096f89450a4c2ffb6e254776e0c
Giacom0Mancuso <giacomomancuso35@email.com>
2023-12-06 17:19:40 +0100

    work done

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,2 +1,27 @@
-package com.ferragnez.party;public class CheckGuest {
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+    public static void main(String[] args) {
+        String[] inviti = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        boolean check = false;
+        for (int i = 0; i < inviti.length; i++){
+            Scanner scanner = new Scanner(System.in);
+            String guest = scanner.nextLine();
+            for (int j = 0; j < inviti.length; j++) {
+                if (guest.equals(inviti[j])) {
+                    System.out.println("prego può entrare");
+                    check = true;
+                }
+            }
+            if (!check){
+                System.out.println("lei non è sulla lista");
+            }
+            if (check){
+                check = false;
+            }
+        }
+    }
 }
